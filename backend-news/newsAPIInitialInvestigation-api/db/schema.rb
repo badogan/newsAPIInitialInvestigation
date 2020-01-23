a70f6e299aaff653586c64a32eb44bf18dc5103a
badogan <basri.dogan.71@gmail.com>
2020-01-23 08:27:02 +0000

    backend db moved to postgres. presursor work to move to AWS RDS

--- backend-news/newsAPIInitialInvestigation-api/db/schema.rb
+++ backend-news/newsAPIInitialInvestigation-api/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_21_130842) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "user_sessions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_01_21_130842) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.json "saveforlater", default: []
+    t.json "saveforlater", default: [], array: true
   end
 
 end
